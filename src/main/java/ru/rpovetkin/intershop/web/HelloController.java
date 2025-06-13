eedf0c2d42e6c2c203ef7b89d5374800889ab529
Roman Povetkin <1260@smsfinance.ru>
2025-06-13 09:59:33 +0700

    dev: init commit

---
+++ src/main/java/ru/rpovetkin/intershop/web/HelloController.java
@@ -0,0 +1,16 @@
+package ru.rpovetkin.intershop.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String sayHello() {
+        log.info("Публикуем ответ...");
+        return "Добро пожаловать на борт!";
+    }
+}
